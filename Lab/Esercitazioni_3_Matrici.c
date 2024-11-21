c8ddf55ca7c0984cac0b9175e8a65b783dfc30a9
Pietro Panzeri <pietro.panzeri@issgreppi.it>
2024-11-21 14:58:29 +0100

    Prodotto riga colonna

--- Lab/Esercitazioni_3_Matrici.c
+++ Lab/Esercitazioni_3_Matrici.c
@@ -68,14 +68,73 @@ int SommaMatrice(const int matrice[][numCol], int righe, int colonne){
 
 }
 
+void MassimoPerRiga(const int matrice[][numCol], int righe, int colonne, int massimo[]){
+     
+    printf("Max di ogni riga: \n");
+
+    for (int i = 0; i < righe; i++)
+    {
+        int max = matrice[i][0];
+        for (int j = 0; j < colonne; j++)
+        {
+            if(max < matrice[i][j])
+                max = matrice[i][j];
+        }
+        printf("Riga %d:\n", i);
+        printf("Massimo: %d\n", max);
+        massimo[i] = max;
+    }
+    return;
+}
+
+void TraspostaMatrice(const int matrice[][numCol], int trasposta[][numRow], int righe, int colonne){
+
+    for (int i = 0; i < righe; i++)
+    {
+        for (int j = 0; j < colonne; j++)
+        {
+            trasposta[j][i] = matrice[i][j];
+        }
+    }
+    return;
+}
+
+int ProdottoRigaColonna(const int matrice[][numCol], int righe, int colonne, int riga, int colonna){
+    
+    int p1 = 1, p2 = 1;
+    
+    for (int i = 0; i < colonna; i++)
+        p1 *= matrice[riga][i];
+        
+    for (int i = 0; i < riga; i++)
+        p2 *= matrice[i][colonna];
+
+    return p1 + p2;
+}
 
 int main () {
 
     int matrice[numRow][numCol];
+    int trasposta[numCol][numRow];
+    int max[numCol];
 
-    LeggiMatrice(matrice, 2, 2);
-    StampaMatrice(matrice, 2, 2);
+    int row = 3;
+    int col = 4;
+
+
+    LeggiMatrice(matrice, row, col);
+    StampaMatrice(matrice, row, col);
     
-    printf("Somma: %d", SommaMatrice(matrice, 2, 2));
+    printf("Somma: %d", SommaMatrice(matrice, row, col));
+
+    MassimoPerRiga(matrice, row, col, max);
+
+    TraspostaMatrice(matrice, trasposta, row, col);
+    StampaMatrice(trasposta, col, row);
+
+    printf("Prodotto riga colonna: %d", ProdottoRigaColonna(matrice, row, col, 1, 1));
+
+    
+
     return 0;
 }
