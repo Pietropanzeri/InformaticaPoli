75fd794642e3f94e804dc8c16c4e2fd25d8ee7f8
Pietro Panzeri <pietro.panzeri@issgreppi.it>
2024-11-21 14:28:06 +0100

    Somma matrici

--- Lab/Esercitazioni_3_Matrici.c
+++ Lab/Esercitazioni_3_Matrici.c
@@ -1,8 +1,81 @@
 #include <stdio.h>
+#include <string.h>
+#define numCol 10
+#define numRow 10
+
+void LeggiMatrice(int matrice[][numCol], int righe, int colonne){
+
+    if(righe > numRow)
+        righe = numRow;
+    if(colonne > numCol)
+        colonne = numCol;
+    
+    printf("Inserisci I valori della matrice: \n");
+    for (int i = 0; i < righe; i++)
+    {
+        printf("Righa %d:\n", i + 1);
+        for (int j = 0; j < colonne; j++)
+        {
+            scanf("%d", &matrice[i][j]);
+        }   
+    }
+    
+    return;
+}
+
+void StampaMatrice(const int matrice[][numCol], int righe, int colonne){
+    
+    char separatore[200] = "";
+    char separatore2[200] = "";
+
+    for (int i = 0; i < colonne; i++){
+        strcat(separatore2, "|---------------");
+        strcat(separatore , "----------------");
+    }
+
+    strcat(separatore2, "|\n");
+    strcat(separatore , "-\n");
+
+    //Stampa
+    printf("\n");
+    for (int i = 0; i < righe; i++)
+    {
+        printf("%s", separatore2);
+        for (int j = 0; j < colonne; j++)
+        {
+            printf("|\t%d\t", matrice[i][j]);
+        }
+        printf("|\n");
+    }
+    printf("%s\n", separatore2);
+    return;
+}
+
+int SommaMatrice(const int matrice[][numCol], int righe, int colonne){
+
+    int somma = 0;
+
+    for (int i = 0; i < righe; i++)
+    {
+        for (int j = 0; j < colonne; j++)
+        {
+            somma += matrice[i][j];
+        }
+        
+    }
+    
+    return somma;
+
+}
+
 
 int main () {
 
+    int matrice[numRow][numCol];
 
+    LeggiMatrice(matrice, 2, 2);
+    StampaMatrice(matrice, 2, 2);
     
+    printf("Somma: %d", SommaMatrice(matrice, 2, 2));
     return 0;
 }
