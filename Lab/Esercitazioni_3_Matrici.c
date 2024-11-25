9675132176488b779f5d86aa8432b3d7b1dc3b6a
Pietro Panzeri <pietro.panzeri@issgreppi.it>
2024-11-25 11:05:46 +0100

    Fine es matrici (forse)
    Da controllare prodotto matrici

--- Lab/Esercitazioni_3_Matrici.c
+++ Lab/Esercitazioni_3_Matrici.c
@@ -119,27 +119,82 @@ int ProdottoRigaColonna(const int matrice[][numCol], int righe, int colonne, int
     return somma;
 }
 
+int Determinante2x2(const int matrice[][numCol]){
+
+    //L'esercizio non ciede di controllare, non lo farò
+    return((matrice[0][0] * matrice[1][1]) - (matrice[0][1] * matrice[1][0]));
+
+}
+
+int VerificaSimmetria(const int matrice[][numCol], int n){
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(matrice[i][j] != matrice[j][i])
+                return 0;
+        }
+        
+    }
+    return 1;
+
+    //potri farlo usando il metodo trasposta e controllando che siano uguali
+}
+
+
+//FORSE CANNATO
+void MoltiplicaMatrici(const int matrice1[][numCol], int righe1, int colonne1, const int matrice2[][numCol], int righe2, int colonne2, int ris[][numCol]){
+    
+    int a = 0;
+
+    if(colonne1 != righe2)
+        printf("Non accettabili");
+
+    for (int i = 0; i < righe1; i++)
+    {
+        for(int h = 0; h < colonne2;h++){
+            for (int j = 0; j < righe2; j++)
+            {
+                a += matrice1[i][j] * matrice2[j][h];
+            }
+            ris[i][h] = a;
+            a = 0;
+        }
+    }
+    
+    StampaMatrice(ris, righe1, colonne2);
+
+}
+
 int main () {
 
     int matrice[numRow][numCol];
     int trasposta[numCol][numRow];
+    int ris[numRow][numCol];
     int max[numCol];
 
     int row = 3;
-    int col = 4;
+    int col = 3;
 
 
     LeggiMatrice(matrice, row, col);
     StampaMatrice(matrice, row, col);
     
-    printf("Somma: %d", SommaMatrice(matrice, row, col));
+    //printf("Somma: %d", SommaMatrice(matrice, row, col));
 
-    MassimoPerRiga(matrice, row, col, max);
+    //MassimoPerRiga(matrice, row, col, max);
 
     TraspostaMatrice(matrice, trasposta, row, col);
     StampaMatrice(trasposta, col, row);
 
-    printf("Prodotto riga colonna: %d", ProdottoRigaColonna(matrice, row, col, 1, 1));
+    //printf("Prodotto riga colonna: %d", ProdottoRigaColonna(matrice, row, col, 1, 1));
+
+    //printf("Determinate 2x2: %d", Determinante2x2(matrice));
+
+    //printf("Simmetria: %d", VerificaSimmetria(matrice, row));
+
+    MoltiplicaMatrici(matrice, row, col, trasposta, col, row, ris);
 
     return 0;
 }
