68a952f7f15d38aa567c51379d139ebf2f8121c6
Pietro Panzeri <pietropanzeri005@gmail.com>
2024-12-07 11:31:57 +0100

    Esercitazioni 5 Push Pop Visualizza

--- Lab/Esercitazioni_5_12-12.c
+++ Lab/Esercitazioni_5_12-12.c
@@ -4,40 +4,25 @@
 typedef struct EL { 
         int info;
         struct EL * prox;
+
+        int (*Pop)(struct EL**);
+        void (*Push)(struct EL**, int val);
+        void (*VisualizzaEliminaPila)(struct EL**);
 }ElemDiLista;
 
 typedef ElemDiLista * ListaDiElem;
 
-void InserimentoValori(ListaDiElem * testa){
-    int ins = 4;
-    ElemDiLista * corrente = *testa;
-    printf("Inserisci i valori che vuoi inserire nella lista, termina con 0\n");
-    while (ins != 0)
-    {
-        ins = ins-1;
-        printf("Inserisci:\n");
-        //scanf("%d", &ins);
-        ElemDiLista * elemento = malloc(sizeof(ElemDiLista));
-        if(elemento == NULL)
-        {
-            printf("Errore nell'allocazione della memoria\n");
-            return;
-        }
-        elemento->info = ins;
-        elemento->prox = NULL;
 
-        if(*testa == NULL)
-        {
-            *testa = elemento;
-            corrente = *testa;
-        }
-        else
-        {
-            corrente->prox = elemento;
-            corrente = corrente->prox;
-        }
-    }
-    
+int Pop(ListaDiElem * testa){
+    if(*testa == NULL)
+        return 0;
+    ElemDiLista * app;
+    *testa = app;
+
+    *testa = (*testa)->prox;
+    int val = app->info;
+    free(app);
+    return val;
 }
 
 void StampaLista(ListaDiElem testa){
@@ -66,6 +51,64 @@ void EliminaLista(ListaDiElem * testa){
     return;
 }
 
+void VisualizzaEliminaPila(ListaDiElem * testa){
+    StampaListaPlus(*testa, "-----\nUltima Stampa Della Lista prima di Eliminare:");
+    EliminaLista(&(*testa));
+}
+
+void Push(ListaDiElem * testa, int val){
+    if(*testa == NULL)
+        return;
+
+    ElemDiLista * elemento = malloc(sizeof(ElemDiLista));
+    if(val != 0)
+    {
+        elemento->info = val;
+        elemento->prox = *testa;
+        elemento->Pop = Pop;
+        elemento->Push = Push;
+        elemento->VisualizzaEliminaPila = VisualizzaEliminaPila; 
+        *testa = elemento;
+    }
+}
+
+void InserimentoValori(ListaDiElem * testa){
+    int ins = 4;
+    ElemDiLista * corrente = *testa;
+    printf("Inserisci i valori che vuoi inserire nella lista, termina con 0\n");
+    
+    printf("Inserisci:\n");
+    scanf("%d", &ins);
+    while (ins != 0)
+    {
+        ElemDiLista * elemento = malloc(sizeof(ElemDiLista));
+        if(elemento == NULL)
+        {
+            printf("Errore nell'allocazione della memoria\n");
+            return;
+        }
+        elemento->info = ins;
+        elemento->prox = NULL;
+        elemento->Pop = Pop;
+        elemento->Push = Push;
+        elemento->VisualizzaEliminaPila = VisualizzaEliminaPila;  
+
+        if(*testa == NULL)
+        {
+            *testa = elemento;
+            corrente = *testa;
+        }
+        else
+        {
+            corrente->prox = elemento;
+            corrente = corrente->prox;
+        }
+        printf("Inserisci:\n");
+        scanf("%d", &ins);
+    }
+    
+}
+
 void EliminaNum(ListaDiElem * testa, int num){
     ElemDiLista * current = *testa;
 
@@ -78,7 +121,7 @@ void EliminaNum(ListaDiElem * testa, int num){
         return;
     }
 
-    while (current->prox != NULL && current->prox->info != num)
+    while (current->prox != NULL && current->prox->info != num )
         current = current->prox;
     
     if(current->prox == NULL)
@@ -112,24 +155,36 @@ void InserisciInLista(ListaDiElem * testa, int val, int index){
     ElemDiLista * nuovo = malloc(sizeof(ElemDiLista));
     nuovo->info = val;
     nuovo->prox = current->prox;
+    nuovo->Pop = Pop;
+    nuovo->Push = Push;
+    nuovo->VisualizzaEliminaPila = VisualizzaEliminaPila;  
     current->prox = nuovo;
     StampaListaPlus(*testa,  "-----");    
 }
-
-//Esercizio 5 da fare
 
 int main (int argc, char *argv[]) {
 
     ListaDiElem Lista = NULL; 
 
     InserimentoValori(&Lista);
-    StampaListaPlus(Lista,  "-----");
+    StampaListaPlus(Lista, "-----");
     
-    EliminaNum(&Lista, 0);
+    printf("Valore da eliminare?\n");
+    int val;
+    scanf("%d", &val);
+    EliminaNum(&Lista, val);
 
     InserisciInLista(&Lista, 9, 2);
 
-    EliminaLista(&Lista);
+    Lista->Push(&Lista, 5);
+    StampaListaPlus(Lista, "-----");
+
+    printf("Rimosso il: %d\n", Lista->Pop(&Lista));
+    StampaListaPlus(Lista, "-----");
+
+    Lista->VisualizzaEliminaPila(&Lista);
+
+    StampaListaPlus(Lista, "-----");
 
     return 0;
 }
