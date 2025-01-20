4ccfc4708d5c2048293f473549ca1a9d15e25bb2
Pietro Panzeri <pietropanzeri005@gmail.com>
2025-01-20 11:24:38 +0100

    Es 4 prova 07-11
    Se mai luca leggerai questo commento, si non è ottimizzata ma almeno funziona e non ho intenzione di starci a pensare oltre

---
+++ Temi_Esame/Cagliotti2022-07-11.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+#define Dim 5
+
+int MatriceBinaria(int matrice[][5]){
+    int val = 0;
+    int cont = 0;
+    int maxcont = 0;
+    int posizione = 0;
+    int j = 0;
+    int maxorder = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        //forse posso usare una solo variabile invece che j e cont
+        while (j < 5)
+        {
+            if(matrice[i][j] == 1)
+            {
+                cont++;
+                if(maxcont <= cont)
+                {
+                    maxcont = cont;
+                    if(cont == 1)
+                        posizione = j;  //posizione corrisponde alla colonna in cui iniziano gli 1
+                }
+            }
+            else
+                cont = 0;
+            
+            j++;
+        }
+        //uso j per una altro scopo
+        j = 0;
+        for (j = i; j < maxcont + i; j++)
+        {
+            for (int h = posizione; h < maxcont + posizione; h++)
+            {
+                if(h < 5 && j < 5 && matrice[j][h] == 1)
+                    val++;
+            }
+        }
+        if(val == maxcont*maxcont && maxcont > maxorder){
+            maxorder = maxcont;
+        }
+        val = 0;  
+        j = 0;
+        maxcont = 0;
+    }
+    return maxorder;
+}
+
+
+int main(){
+    
+    int matrice[5][5] = {
+        {1, 1, 1, 0, 0},
+        {1, 1, 0, 0, 0},
+        {1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int order = MatriceBinaria(matrice);
+
+    printf("%d", order);
+
+    return 0;
+}
